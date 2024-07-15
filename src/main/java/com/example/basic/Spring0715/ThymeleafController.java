f79f712ae6fe687bd6c5824c42fae47bb51cf8dc
MoonSeokJun <tjrwns2513@naver.com>
2024-07-15 14:47:43 +0900

    스프링 0715 오후

--- src/main/java/com/example/basic/Spring0715/ThymeleafController.java
+++ src/main/java/com/example/basic/Spring0715/ThymeleafController.java
@@ -14,7 +14,7 @@ public class ThymeleafController {
 
     @GetMapping("/index")
     public String index(Model model) {
-        User max = new User("max", "max@gmail.com", false, false);
+        User max = new User("max", "max@gmail.com", false, false, "max");
         int[] iterData = {1, 2, 3, 4, 5};
         model.addAttribute("user", max);
         model.addAttribute("iterData", iterData);
@@ -33,7 +33,7 @@ public class ThymeleafController {
 
     @PostMapping("/users")
     public String user(@ModelAttribute User user) {
-        System.out.println("유저의 이름은" + user.getName() + "입니다.");
+        System.out.println("유저의 이름은" + user.getUsername() + "입니다.");
         System.out.println("유저의 이메일은" + user.getEmail() + "입니다.");
 
         return "index2";
